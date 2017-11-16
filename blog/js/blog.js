02af438f76e0a3aa9820a80baa0df0865f56149d
junmeng1 <mengjun2010@gmail.com>
2017-11-15 18:45:15 -0800

    changed "document.ready"

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -1,28 +1,22 @@
-$(document).ready(function(){ //why not putting event in there?
+$(document).ready(function(){ 
   $('.readmore').click(function(event){
   	event.preventDefault();
     $('#show-this-on-click').slideDown();
     $('.readmore').hide();
     $('.readless').show();
-    
     }) 
+
+    $('.readless').click(function(event){
+      event.preventDefault();
+      $('#show-this-on-click').slideUp();
+      $('.readmore').show();
+      $('.readless').hide();
+    })
+    
+    $('.learnmore').click(function(event){
+      event.preventDefault();
+      $('#learnmoretext').slideDown();
+      $('.learnmore').hide();
+    });
 });
 
-$(document).ready(function(){
-  $('.readless').click(function(event){
-  	event.preventDefault();
-  	$('#show-this-on-click').slideUp();
-    $('.readmore').show();
-    $('.readless').hide();
-    
-  })
-});
-
-$(document).ready(function(){
-  $('.learnmore').click(function(event){
-  	event.preventDefault();
-  	$('#learnmoretext').slideDown();
-    $('.learnmore').hide();
-    
-  })
-});
